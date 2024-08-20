9b33250b32df8e5965bd49a5d0ff547ad9fa8c16
Lucas Lima <lukenoutte@gmail.com>
2024-08-20 13:34:27 -0300

    fix/db env

--- src/main/configs/env.ts
+++ src/main/configs/env.ts
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 
 dotenv.config({ path: ".env" });
 
-const postgreUrl = process.env.POSTGRE_URL as string;
+const postgreUrl = process.env.DATABASE_URL as string;
 const serverPort = process.env.PORT || 3000;
 const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET as string;
 const refreshTokenSecret = process.env.REFRESH_TOKEN_SECRET as string;
