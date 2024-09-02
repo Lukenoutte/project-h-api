64bdf1958563bd7193fadd50439ef2d040abb1d9
Lucas Lima <lukenoutte@gmail.com>
2024-09-02 11:20:44 -0300

    fix/find store

--- src/main/routes/index.ts
+++ src/main/routes/index.ts
@@ -1,4 +1,5 @@
 import signUpStoreRoute from "./stores/signup-store-route";
+import findStoreRoute from "./stores/find-store-route";
 import signUpUserRoute from "./users/signup-user-route";
 import findUserRoute from "./users/find-user-route";
 import signInRoute from "./authentication/signin-route";
@@ -6,10 +7,11 @@ import signOutRoute from "./authentication/signout-route";
 import refreshTokenRoute from "./authentication/refesh-token-route";
 
 export default [
+  findStoreRoute,
   signUpStoreRoute,
   signUpUserRoute,
   findUserRoute,
   signInRoute,
   signOutRoute,
-  refreshTokenRoute,
+  refreshTokenRoute
 ];
--- src/main/routes/stores/find-store-route.ts
+++ src/main/routes/stores/find-store-route.ts
@@ -1,10 +1,10 @@
+import FindStoreRouterComposer from "main/composers/stores/find-store-router-composer";
 import ExpressRouterAdapter from "../../adapters/express-router-adapter";
-import SignUpStoreRouterComposer from "../../composers/stores/signup-store-router-composer";
 import { Router } from "express";
 
 export default (router: Router) => {
   router.get(
     "/store",
-    ExpressRouterAdapter.adapt(SignUpStoreRouterComposer.compose()),
+    ExpressRouterAdapter.adapt(FindStoreRouterComposer.compose()),
   );
 };
